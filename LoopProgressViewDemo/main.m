78ad77e127819d4216ac2e99fdc7d855a5c1b684
CUG <cug@CUGdeMacBook-Pro.local>
2016-09-13 18:55:24 +0800

    Initial Commit

---
+++ LoopProgressViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LoopProgressViewDemo
+//
+//  Created by CUG on 16/9/13.
+//  Copyright © 2016年 CUG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
